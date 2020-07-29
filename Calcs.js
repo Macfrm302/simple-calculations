e28e7ea2b104ab786d9d67c42a3179b56df2b3fe
Mac_frm302 <malachichamlee77@gmail.com>
2020-07-29 16:35:54 -0400

    mathV2

---
+++ Calcs.js
@@ -0,0 +1,25 @@
+console.log("IamLucid");
+
+console.log(12+30);
+
+console.log(12%2);
+
+// console.log(3*3);
+
+// console.log(3-5);
+
+var x = 45;
+var y = 39;
+
+// console.log(x+y);
+
+// console.log(x-y);
+
+// console.log(x/y);
+
+// console.log(x*y);
+
+for(var i=5; i<=15; i+=2){
+    console.log(i);
+}
+
