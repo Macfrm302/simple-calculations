b4972e86cca1d445b69bf7b11579b4db26bd044d
Mac_frm302 <malachichamlee77@gmail.com>
2020-07-30 16:29:45 -0400

    bruh my brain cells

--- Calcs.js
+++ Calcs.js
@@ -23,3 +23,30 @@ for(var i=5; i<=15; i+=2){
     console.log(i);
 }
 
+var bigmac = "hi my name is macfrm302 or malachi";
+var bruh = "i inspire to be god at skateboarding like Aaron Kyro";
+
+
+console.log 
+
+console.log("Apex legends");
+
+for(var i=5;i<=15; i+=2){
+    console.log(i)                                                       
+}
+
+var firstname = "mac"
+var lastname = "frm302"
+
+var fullname = firstname.concat(lastname);
+
+
+console.log(fullname);
+
+for(var i=1; i<=100; i++){
+    if(i % 3 == 0){
+        console.log("swaggersouls")
+    } else{
+        console.log ("Bruh")
+    }
+}
